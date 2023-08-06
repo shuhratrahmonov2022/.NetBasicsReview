3791ec98970ffe2eab3e595b85789c96acf91ca4
Shuhratjon <shuhratrahmonov2022@gmail.com>
2023-08-06 11:46:17 +0500

    Debugging

---
+++ 02.Lesson/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Age;
+            try
+            {
+                Console.Write("Enter your age: ");
+                Age = Convert.ToInt32(Console.ReadLine());
+                if (Age <= 12)
+                {
+                    Console.WriteLine("You do not have access!");
+                }
+                else
+                {
+                    Console.WriteLine("Welcome");
+                }
+            }
+            catch (DivideByZeroException dex)
+            {
+                Console.WriteLine(dex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
