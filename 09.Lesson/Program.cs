4ae2c90d47592f6f9b2111e6f5404620f9d238b4
Shuhratjon <shuhratrahmonov2022@gmail.com>
2023-08-10 10:52:00 +0500

    intit methods

---
+++ 09.Lesson/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace lesson09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WriteString("Assalom aleykum");
+
+            //string a = WriteString("Assalom", "aleykum");
+            //Console.WriteLine(a);
+
+            // Starter();
+
+            int intVar = 5;
+            Console.WriteLine(intVar);
+            TwoMultiply(ref intVar);
+            Console.WriteLine(intVar);
+        }
+
+        //Ex. 1
+        //Prosedure
+        static void WriteString(string a)
+        {
+            Console.WriteLine(a + "!");
+        }
+
+        //Function
+        static string WriteString(string a, string b) 
+        {
+            return a + b;
+        }
+
+        //Ex. 2
+        //static void Starter()
+        //{
+        //    string word = Console.ReadLine();
+        //    Console.WriteLine("you wrote it {0}",word);
+        //}
+
+        //Ex. 3
+        static void TwoMultiply(ref int intVar)
+        {
+            intVar = intVar * 2;
+        }
+    }
+}
