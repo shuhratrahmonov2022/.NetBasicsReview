3b8bbd0c90af43321a586670f41ac1d9de54e572
Shuhrat Rahmonov <shuhrat@silkroadprofessionals.com>
2023-08-12 22:56:35 +0500

    partial arraylists

---
+++ 12.Lesson/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+
+namespace Lesson11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ArrayList, Hashtable, List, Queue, Stack, Dictionary
+
+            //Ex. 1
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add("one");
+            arrayList.Add("two");
+            arrayList.Add("three");
+            Console.WriteLine("ArrayList data: " + arrayList.Count);
+
+            arrayList.Insert(3, "Four");
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Ex. 2 
+            arrayList.RemoveAt(1);
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+
+            arrayList.Clear();
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
